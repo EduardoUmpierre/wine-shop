ae4c3a3e7f58b625e523eac875a939aa5895af45
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-01 16:36:12 -0300

    Customer service added

---
+++ src/app/customer.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/index';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CustomerService {
+
+    constructor(private http: HttpClient) {
+    }
+
+    getAll(): Observable<any> {
+        return this.http.get('http://www.mocky.io/v2/598b16291100004705515ec5');
+    }
+
+    /**
+     * Gets the customers CPF without the special characters
+     * @param {string} cpf
+     * @returns {string}
+     */
+    getCleanCpf(cpf: string) {
+        return cpf.replace(/[\.\-]/g, '');
+    }
+}
