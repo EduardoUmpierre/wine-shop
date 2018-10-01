a84478c5d072a24a98e3bacc0d2cc07588458360
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2018-10-01 16:41:49 -0300

    Get customer ordered by total list added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CustomerService } from './customer.service';
+import { ShopHistoryService } from './shop-history.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wine-shop';
+export class AppComponent implements OnInit {
+    orderedCustomersByTotal: any[] = null;
+
+    constructor(private customerService: CustomerService, private shopHistoryService: ShopHistoryService) {
+    }
+
+    /**
+     *
+     */
+    ngOnInit() {
+        this.getCustomersOrderedByTotal();
+    }
+
+    /**
+     * Gets all customers ordered by the total of them purchases
+     */
+    private getCustomersOrderedByTotal() {
+        this.customerService.getAll().subscribe((customers) => {
+
+            this.shopHistoryService.getAll().subscribe((historic) => {
+
+                customers.forEach((customer, index) => {
+                    // Get shopping history by customer
+                    const customerShopHistory = historic.filter(item => {
+                        const customerCpf = this.customerService.getCleanCpf(customer.cpf);
+                        const historicItemCustomerCpf = this.shopHistoryService.getCleanCustomerCpf(item.cliente);
+
+                        return historicItemCustomerCpf === customerCpf;
+                    });
+
+                    // Sum the shopping history
+                    customers[index]['total'] = customerShopHistory.reduce((previousValue, element) => {
+                        return previousValue + element.valorTotal;
+                    }, 0).toFixed(2);
+                });
+
+                // Sort customers by shopping history
+                this.orderedCustomersByTotal = customers.sort((a, b) => b['total'] - a['total']);
+            });
+        });
+    }
 }
