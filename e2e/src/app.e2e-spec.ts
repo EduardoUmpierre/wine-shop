4fc1848c497b111357366a66c05c0900be09a1cf
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2018-10-01 23:19:43 -0300

    Basic e2e tests added

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,26 @@
 import { AppPage } from './app.po';
 
 describe('workspace-project App', () => {
-  let page: AppPage;
+    let page: AppPage;
 
-  beforeEach(() => {
-    page = new AppPage();
-  });
+    beforeEach(() => {
+        page = new AppPage();
+    });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to wine-shop!');
-  });
+    it('should display page title', () => {
+        page.navigateTo();
+        expect(page.getPageTitle()).toEqual('Wine shop');
+    });
+
+    it('should display the correct title', () => {
+        expect(page.getCustomersOrderedByShopHistory()).toEqual('Lista de clientes pelo valor total de compras');
+    });
+
+    it('should display the correct title', () => {
+        expect(page.getCustomerWithBiggestPurchase()).toEqual('Cliente com maior compra no último ano');
+    });
+
+    it('should display the correct title', () => {
+        expect(page.getMostLoyalCustomers()).toEqual('Clientes mais fiéis');
+    });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,23 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+    navigateTo() {
+        return browser.get('/');
+    }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
+    getPageTitle() {
+        return element(by.css('body > .container > h1')).getText();
+    }
+
+    getCustomersOrderedByShopHistory() {
+        return element(by.css('app-root > .row > .col-12:first-child > app-customer-list > h2')).getText();
+    }
+
+    getCustomerWithBiggestPurchase() {
+        return element(by.css('app-root > .row > .col-12:last-child > app-customer-list:first-child > h2')).getText();
+    }
+
+    getMostLoyalCustomers() {
+        return element(by.css('app-root > .row > .col-12:last-child > app-customer-list:last-child > h2')).getText();
+    }
 }
